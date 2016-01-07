2fb4d6f3f505d45e8aff318e31a69e55e709c58d
mhakopian <mhakopian@gmail.com>
2016-01-07 08:45:01 +0100

    observers added

--- app/code/local/Jexia/Eventdemo/Model/observer.php
+++ app/code/local/Jexia/Eventdemo/Model/observer.php
@@ -1,13 +1,9 @@
 <?php
 class Jexia_Eventdemo_Model_Observer {
-  public function Mytestmethod($observer) {
-    $event = $observer->getEvent(); //Fetches the current event
-    $product = $event->getProduct();
-    $eventmsg = "Current Event Triggered : " . $event->getName() . "Currently Added Product : " . $product->getName();
-    //Adds Custom message to shopping cart
-    echo Mage::getSingleton('checkout/session')->addSuccess($eventmsg);
-    //Your Custom Logic Here
-    //you can use print_r($product) here to get more details
-    print_r($product);
+
+  public function logCustomer($observer) {
+    $customer=$observer->getCustomer();
+    Mage::log('['.date('dd-mm-yyyy H:i:s').']'.$customer->getName.' Logged in at: ');
   }
+
 }

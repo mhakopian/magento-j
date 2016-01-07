3c1e1e51c4cce88aab090f1257d84a3e42ff909a
mhakopian <mhakopian@gmail.com>
2016-01-07 09:34:22 +0100

    observers added1

--- app/code/local/Jexia/Eventdemo/Model/observer.php
+++ app/code/local/Jexia/Eventdemo/Model/observer.php
@@ -4,7 +4,9 @@ class Jexia_Eventdemo_Model_Observer {
   public function logCustomer($observer) {
     $customer=$observer->getCustomer();
     //print_r($customer);
-    Mage::log('['.date('dd-mm-yyyy H:i:s').']'.$customer->getName.' Logged in at: ',null, 'jexia.log');
+    Mage::log('['.date('dd-mm-yyyy H:i:s').']'.$customer->getName.' logged-in');
+
+    return $this;
   }
 
 }
